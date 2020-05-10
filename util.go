63f04e1f8786d4610ea24d66589f4b5b23d232f3
Matiss Kiris <matiss.kiris@gmail.com>
2020-05-10 11:22:56 +0300

    Pass initial checksum value as uint16 to function

--- util.go
+++ util.go
@@ -1,8 +1,8 @@
 package motronic
 
 // Checksum16bit
-func Checksum16bit(initial uint32, start int, end int, buf []byte) uint16 {
-	var sum uint32 = initial
+func Checksum16bit(initial uint16, start int, end int, buf []byte) uint16 {
+	var sum uint32 = uint32(initial)
 
 	for i := start; i <= end; i += 1 {
 		sum += uint32(buf[i])
